f50a997d2d5a46a5d6668f1066db393e3179c42e
Arnav Kumar Sahoo <115882135+sahooarnavkumar@users.noreply.github.com>
2023-05-26 16:37:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 from flask import Flask, render_template, Response
 import cv2
-from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import datetime
+import math
+from cvzone.PoseModule import PoseDetector as pm
 
 app = Flask(__name__)
-camera = cv2.VideoCapture(0)
-
-# Initialize hand detector
-detector = HandDetector(detectionCon=0.8, maxHands=2)
+cap = cv2.VideoCapture(0)
+detector = pm()
 
 def generate_frames():
     while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        
-        # Perform posture detection on the frame
-        frame = detector.findHands(frame)
-        # ...
-        # Perform other operations on the frame if needed
-        # ...
+        success, img = cap.read()
+        img = cv2.resize(img, (1280, 720))
+        img = detector.findPose(img)
+        lmList, bboxInfo = detector.findPosition(img, draw=False)
 
-        # Convert the frame to JPEG format
-        ret, buffer = cv2.imencode('.jpg', frame)
+        ret, buffer = cv2.imencode('.jpg', img)
         frame = buffer.tobytes()
 
         yield (b'--frame\r\n'
@@ -37,9 +32,17 @@ def index():
 def video():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def stop_recording():
-    # Code to stop the recording goes here
-    pass
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/stop_recording')
+def stop_recording():
+    global output_video
+
+    if output_video is not None:
+        output_video.release()  # Release the video writer
+        output_video = None
+
+    return 'Video recording stopped.'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
